f343d7c61495f306d1498134c8b46ee2f1ad6eda
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-05-18 07:26:27 -0700

    Free Memory

---
+++ _freesh.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+/**
+ * _free - Free the memory
+ * @d: A pointer
+ * @e: An integer
+ *
+ * Return: 0
+ */
+void _free(char **d, int e)
+{
+	int f;
+
+	for (f = 0; f < e; f++)
+	{
+		free(d[f]);
+	}
+	if (d == NULL)
+		return;
+}
