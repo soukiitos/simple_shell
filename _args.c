aeafc6818e030fc42dc8ed0f187f2ea38df62727
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-05-18 10:47:42 -0700

    Array of Arguments

---
+++ _args.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+/**
+ * _args - An array of arguments
+ * @c: A character
+ * @args: A pointer
+ *
+ * Return: 0
+ */
+int _args(char *c, char **args)
+{
+	char *i, *j = c, *k;
+	int a;
+
+	k = strtok(c, "\n\t\r ");
+	/** strtok() returns a pointer to the first token found in the string */
+	a = 0;
+	args[a] = k;
+	for (a = 0; !j; a++)
+	{
+		j = i;
+		i = strtok(NULL, "\n\t\r ");
+		args[a] = i;
+	}
+	args[a] = NULL;
+	return (a);
+}
