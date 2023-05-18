dfcf7d14bb072de0e0526fce41088f0b556c2466
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-05-18 06:39:16 -0700

    The Main Header of our Simple Shell Project

---
+++ shell.h
@@ -0,0 +1,28 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+extern char **ext_var;
+
+int _putchar(char c);
+int beautsh(const char *a, unsigned int b);
+int args(char *m, char **c);
+int execute(char **c);
+void _free(char **d, int e);
+void _verfree(char **c);
+int com_exist(char *path);
+int _verth(char **c);
+int _verpth(char **ch, int k);
+int not_exist(char **args, int i);
+void _cdir(char *dir);
+
+#endif
