cd3392c2009397867ad2ec3762894de0e066917c
yassxyassmine <yassmine.elghalbzouri129@gmail.com>
2023-05-25 01:35:17 -0700

    simple_shell

--- environ.c
+++ environ.c
@@ -1,42 +1,92 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include "shell.h"
 
-#define BUFFER_SIZE 1024
-
-int main(void)
+/**
+ * _myenv - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int _myenv(info_t *info)
 {
-	char command[BUFFER_SIZE];
-	int status;
+	print_list_str(info->env);
+	return (0);
+}
 
-	while (1)
+/**
+ * _getenv - gets the value of an environ variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
 	{
-		printf("$ "); // Display the prompt
-		fflush(stdout); // Flush the output buffer
-
-		// Read the user input
-		if (fgets(command, BUFFER_SIZE, stdin) == NULL)
-		{
-			printf("\n");
-			break; // End of file (Ctrl+D) condition
-		}
-
-		// Remove the trailing newline character
-		command[strcspn(command, "\n")] = '\0';
-
-		// Check if the user wants to exit
-		if (strcmp(command, "exit") == 0)
-			break;
-
-		// Execute the command
-		status = system(command);
-
-		// Check if the command execution was successful
-		if (status != 0)
-			printf("Command execution failed\n");
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
 	}
+	return (NULL);
+}
 
-	return 0;
+/**
+ * _mysetenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _mysetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _myunsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * Return: Always 0
+ */
+int _myunsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list - populates env linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
 }
